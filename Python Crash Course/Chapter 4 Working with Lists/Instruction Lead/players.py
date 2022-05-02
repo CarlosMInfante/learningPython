953fe7b853034ac07aa9dcf857556319cd6ae6b2
CarlosMInfante <83223613+CarlosMInfante@users.noreply.github.com>
2022-05-02 09:23:26 -0400

    Working on better comments

--- Python Crash Course/Chapter 4 Working with Lists/Instruction Lead/players.py
+++ Python Crash Course/Chapter 4 Working with Lists/Instruction Lead/players.py
@@ -1,11 +1,25 @@
+# Working with Part of a List
 # Slicing a List
+# Taking items from the zero element to the 3rd element
 players = ['charles', 'martina', 'michael', 'florence', 'eli']
 print(players[0:3])
+
+# This will take the second, third, and fourth items
+# This print is still pulling from the above list
 print(players[1:4])
+
+# You can also leave out the first index, and it will start at zero automatically
 print(players[:4])
 
-# Looping Through a Slice
-players = ['charles', 'martina', 'michael', 'florence', 'eli']
+# You can also leave out the end index, and it will automatically end at the list end
+print(players[2:])
+
+# You can also use the negative index to get items from the end of the list
+print(players[-3:])
+
+# Looping through a Slice
+# This will loop through players[] and output the first three names
 print("Here are the first three players on my team:")
+# Since we are starting at the beginning, we do not need the first index
 for player in players[:3]:
     print(player.title())
