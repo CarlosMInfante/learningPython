bf9d4969832138e08960b7598b7edd0e82c3aa28
CarlosMInfante <83223613+CarlosMInfante@users.noreply.github.com>
2022-07-07 09:29:17 -0400

    Just messing around, trying to figure this out.

---
+++ zeroToMastery/Pyhon Basics/string.py
@@ -0,0 +1,10 @@
+selfish = 'me me me'
+# 01234567
+
+#  [start:stop:step over]
+print(selfish[1:3])
+
+quote = 'to be or not to be'
+
+print(quote.replace('be', 'me'))  # This replaces only in this print action.
+print(quote)
